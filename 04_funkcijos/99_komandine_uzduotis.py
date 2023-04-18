0726e31c18c7d378a48ea0d2a12ec9fbf72df557
Kestutis Januskevicius <infohata@gmail.com>
2023-04-18 10:13:01 +0300

    funkciju komandine užduotis

---
+++ 04_funkcijos/99_komandine_uzduotis.py
@@ -0,0 +1,32 @@
+'''
+Parašykite programą šaldytuvas, kuri:
+
+- saugo produktus žodyne, kur:
+-- produkto pavadinimas yra raktas
+-- kiekis yra reikšmė float
+
+Tarkime, kad:
+- kieti produktai matuojami kilogramais
+- skysti litrais
+- paruošti patiekalai matuojami vienetais
+tokiu atveju, mums nereikia apibrėžti mato vieneto
+
+Šaldytuvas turi meniu, per kurį galime:
+- įdėti produktus
+- išimti produktus
+-- jeigu produkto kiekis tampa 0, ištriname
+- peržiūrėti produktų sąrašą
+- suskaičiuoti šaldytuvo turinio svorį
+-- skystų tankis = 1
+-- paruoštų patiekalų porcija sveria 1kg
+
+BONUS:
+Įterpti meniu punktą "ar išeina", kur vartotojo prašo įvesti:
+- vienos porcijos receptą
+-- įvedimo formatas: "ingredientas: kiekis" vienoje eilutėje.
+- porcijų kiekį
+Tada programa turėtų:
+- išvesti, ar užtenka ingredientų patiekalams paruošti
+- jeigu neužtenka, išvardinti ko ir kiek trūksta (shopping list).
+- išvesti, kelioms porcijoms užtenka ingredientų, jei yra perteklius
+'''
