a36b3d5564c82bbff19d3fa9585e0794036e7228
Kestutis Januskevicius <infohata@gmail.com>
2023-04-25 11:53:45 +0300

    os modulis PTU12 live

---
+++ PTU12_live/test_os.py
@@ -0,0 +1,20 @@
+import os
+
+print(os.getcwd())
+if os.path.exists("PTU12_live"):
+    os.chdir("PTU12_live")
+    print(os.getcwd())
+if os.path.exists('saldytuvas.json'):
+    print("yra šaldytuvas")
+if not os.path.exists("saldytuvas"):
+    os.makedirs("saldytuvas")
+if os.path.exists("saldytuvas.json"):
+    print("moving fridge....")
+    with open("saldytuvas.json", "r") as senas:
+        with open("saldytuvas/saldytuvas.json", "w") as naujas:
+            naujas.write(senas.read())
+    print("copied.")
+    os.remove("saldytuvas.json")
+    print("deleted.")
+
+print(os.listdir(os.getcwd()))
