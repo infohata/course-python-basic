9b4e887789b3b27e0f32e68eddad705ecc385ab4
Kestutis Januskevicius <infohata@gmail.com>
2023-05-08 14:25:05 +0300

    db1 kurso pavyzdziai, PTU12 live

---
+++ PTU12_live/db_scripts/kursas_db1.sql
@@ -0,0 +1,10 @@
+SELECT * FROM person;
+SELECT first_name, gender FROM person;
+SELECT first_name, gender FROM person ORDER BY gender, first_name;
+SELECT DISTINCT gender FROM person;
+SELECT * FROM person WHERE gender="Male";
+SELECT * FROM person WHERE date_of_birth > DATE("1995-01-01");
+SELECT * FROM person 
+    WHERE gender="Male" 
+    AND date_of_birth < DATE("1995-01-01");
+SELECT * FROM person ORDER BY date_of_birth DESC;
