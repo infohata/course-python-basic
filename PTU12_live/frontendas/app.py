bb6c07adcc9f8816251b7c0254efb2e25efc4a9a
Kestutis Januskevicius <infohata@gmail.com>
2023-05-26 09:54:52 +0200

    html ir css PTU12 live

---
+++ PTU12_live/frontendas/app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/css/')
+def css():
+    return render_template('css.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
