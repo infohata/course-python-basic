9295bb17e471efbb88b0e1c5df4347e7661529d9
Kestutis Januskevicius <infohata@gmail.com>
2023-09-05 16:58:20 +0300

    PTU16 loan calculator done

--- PTU16_live/loan.py
+++ PTU16_live/loan.py
@@ -5,38 +5,62 @@ class Loan():
             self, 
             loan: float, 
             interest_percent: float = 4, 
-            periods: int = 12,
+            duration: int = 12,
             downpayment: float = 0, 
         ) -> None:
         self.loan = loan
         self.downpayment = downpayment
         self.interest_percent = interest_percent
-        self.periods = periods
+        self.duration = duration
         self.principal = loan - downpayment
-        self.increase = self.get_interest()
-        self.repayable = self.principal + self.increase
-        self.remaining = self.repayable
+        self.monthly_true = self.get_monthly_true()
+        self.monthly = self.get_monthly_lowest()
+        self.repayable = round(self.monthly_true * self.duration, 2)
         self.loan_total = downpayment + self.repayable
-        self.monthly, self.penny_months = self.get_monthly_payment()
-    
-    def get_interest(self) -> float:
-        years = self.periods // 12
-        last_year_months = self.periods % years
-        # monthly_principal = self.principal / self.periods
-        compound_interest = []
-        for year in range(years):
-            remaining_years = years - year
-            remaining_principal = self.principal / years * remaining_years
-            compound_interest.append(self.interest_percent / 100 ** remaining_years * remaining_principal)
-        last_year_compound_interest = 0
-        if last_year_months > 0:
-            last_year_compound_interest = (self.principal - (self.principal / years * 12)) / last_year_months 
-        return self.principal * self.interest_percent / 100
+        self.increase = self.repayable - self.principal
+        self.remaining = self.repayable
+        self.penny_months = self.get_penny_months()
+        self.payments = self.get_payment_chart()
 
-    def get_monthly_payment(self) -> (float, int):
-        monthly = self.repayable * 100 // self.periods / 100
-        pennies = (self.repayable / self.periods - monthly) * self.periods * 100
-        return monthly, int(ceil(pennies))
+    def get_monthly_true(self) -> float:
+        monthly_interest = self.interest_percent / 12 / 100
+        monthly = self.principal * monthly_interest / (1 - (1 + monthly_interest) ** -self.duration)
+        return monthly
+    
+    def get_monthly_lowest(self) -> float:
+        return int(self.monthly_true * 100) / 100
+    
+    def get_penny_months(self) -> int:
+        if self.repayable == self.monthly * self.duration:
+            return 0
+        else:
+            pennies = self.repayable - self.monthly * self.duration
+            return int(pennies * 100)
+        
+    def get_monthly(self, month=1):
+        if month <= self.penny_months:
+            return self.monthly + 0.01
+        else:
+            return self.monthly
 
     def get_payment_chart(self) -> list:
-        return
+        payments = []
+        for month in range(self.duration):
+            payments.append(self.get_monthly(month+1))
+        return payments
+
+    def print_loan_info(self) -> None:
+        print(f"""------ Loan stats -----
+            Loan:           {self.loan:10.2f}
+            Downpayment:    {self.downpayment:10.2f}
+            Interest:     {self.interest_percent:10.2f} %
+            Duration:        {self.duration} months
+            Principal:      {self.principal:10.2f}
+            Monthly:        {self.monthly:10.2f}
+            Pennies:        {self.get_penny_months()}
+            Loan Total:     {self.loan_total:10.2f}
+            Increase:       {self.increase:10.2f}\n""")
+
+    def print_monthly_payments(self) -> None:
+        for period, payment in enumerate(self.payments):
+            print(f"{period:3d}: {payment:10.2f}")
--- PTU16_live/test_loan.py
+++ PTU16_live/test_loan.py
@@ -3,25 +3,48 @@ import unittest
 
 
 class TestLoan1(unittest.TestCase):
+    printed = False
+
     def setUp(self) -> None:
         self.loan = Loan(11000, 7, 12, 1000)
+        self.loan.print_loan_info()
+        self.loan.print_monthly_payments()
 
     def test_principal(self):
         self.assertAlmostEqual(self.loan.principal, 10000, 2)
 
     def test_interest(self):
-        self.assertAlmostEqual(self.loan.increase, 700, 2)
+        self.assertAlmostEqual(self.loan.increase, 383.21, 2)
 
     def test_totals(self):
-        self.assertAlmostEqual(self.loan.loan_total, 11700, 2)
-        self.assertAlmostEqual(self.loan.repayable, 10700, 2)
+        self.assertAlmostEqual(self.loan.loan_total, 11383.21, 2)
+        self.assertAlmostEqual(self.loan.repayable, 10383.21, 2)
 
     def test_monthly(self):
-        self.assertAlmostEqual(self.loan.monthly, 891.66, 2)
+        self.assertAlmostEqual(self.loan.monthly, 865.26, 2)
 
-    def test_pennies(self):
-        self.assertEqual(self.loan.penny_months, 8)
-        self.assertAlmostEqual(
-            self.loan.repayable, 
-            self.loan.monthly * 12 + self.loan.penny_months / 100, 2
-        )
+    def test_loan_balance(self):
+        self.assertAlmostEqual(self.loan.repayable, sum(self.loan.payments))
+
+
+class TestLoan2(unittest.TestCase):
+    def setUp(self) -> None:
+        self.loan = Loan(63000, 5, 60, 12600)
+        self.loan.print_loan_info()
+        self.loan.print_monthly_payments()
+
+    def test_principal(self):
+        self.assertAlmostEqual(self.loan.principal, 50400, 3)
+
+    def test_interest(self):
+        self.assertAlmostEqual(self.loan.increase, 6666.61, 3)
+
+    def test_totals(self):
+        self.assertAlmostEqual(self.loan.loan_total, 69666.61, 3)
+        self.assertAlmostEqual(self.loan.repayable, 57066.61, 3)
+
+    def test_monthly(self):
+        self.assertAlmostEqual(self.loan.monthly, 951.11, 3)
+
+    def test_loan_balance(self):
+        self.assertAlmostEqual(self.loan.repayable, sum(self.loan.payments))
