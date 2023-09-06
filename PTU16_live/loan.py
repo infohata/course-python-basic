3c888cadd4c2f8976c51a34d000a0df1670977a7
Kestutis Januskevicius <infohata@gmail.com>
2023-09-06 08:17:43 +0300

    final final loan PTU16

--- PTU16_live/loan.py
+++ PTU16_live/loan.py
@@ -24,7 +24,8 @@ class Loan():
 
     def get_monthly_true(self) -> float:
         monthly_interest = self.interest_percent / 12 / 100
-        monthly = self.principal * monthly_interest / (1 - (1 + monthly_interest) ** -self.duration)
+        monthly = self.principal * monthly_interest \
+            / (1 - (1 + monthly_interest) ** -self.duration)
         return monthly
     
     def get_monthly_lowest(self) -> float:
@@ -57,10 +58,10 @@ class Loan():
             Duration:        {self.duration} months
             Principal:      {self.principal:10.2f}
             Monthly:        {self.monthly:10.2f}
-            Pennies:        {self.get_penny_months()}
+            Pennies:        {self.get_penny_months():10d}
             Loan Total:     {self.loan_total:10.2f}
             Increase:       {self.increase:10.2f}\n""")
 
     def print_monthly_payments(self) -> None:
         for period, payment in enumerate(self.payments):
-            print(f"{period:3d}: {payment:10.2f}")
+            print(f"{period+1:3d}: {payment:10.2f}")
--- PTU16_live/test_loan.py
+++ PTU16_live/test_loan.py
@@ -6,25 +6,25 @@ class TestLoan1(unittest.TestCase):
     printed = False
 
     def setUp(self) -> None:
-        self.loan = Loan(11000, 7, 12, 1000)
+        self.loan = Loan(10000, 7, 12, 0)
         self.loan.print_loan_info()
         self.loan.print_monthly_payments()
 
     def test_principal(self):
-        self.assertAlmostEqual(self.loan.principal, 10000, 2)
+        self.assertAlmostEqual(self.loan.principal, 10000, 3)
 
     def test_interest(self):
-        self.assertAlmostEqual(self.loan.increase, 383.21, 2)
+        self.assertAlmostEqual(self.loan.increase, 383.21, 3)
 
     def test_totals(self):
-        self.assertAlmostEqual(self.loan.loan_total, 11383.21, 2)
-        self.assertAlmostEqual(self.loan.repayable, 10383.21, 2)
+        self.assertAlmostEqual(self.loan.loan_total, 10383.21, 3)
+        self.assertAlmostEqual(self.loan.repayable, 10383.21, 3)
 
     def test_monthly(self):
-        self.assertAlmostEqual(self.loan.monthly, 865.26, 2)
+        self.assertAlmostEqual(self.loan.monthly, 865.26, 3)
 
     def test_loan_balance(self):
-        self.assertAlmostEqual(self.loan.repayable, sum(self.loan.payments))
+        self.assertAlmostEqual(sum(self.loan.payments), 10383.21, 3)
 
 
 class TestLoan2(unittest.TestCase):
@@ -47,4 +47,4 @@ class TestLoan2(unittest.TestCase):
         self.assertAlmostEqual(self.loan.monthly, 951.11, 3)
 
     def test_loan_balance(self):
-        self.assertAlmostEqual(self.loan.repayable, sum(self.loan.payments))
+        self.assertAlmostEqual(sum(self.loan.payments), 57066.61, 3)
