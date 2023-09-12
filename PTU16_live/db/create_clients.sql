4dd440e553dbe5b78e7ba3f06d8b6826760d2fc5
Kestutis Januskevicius <infohata@gmail.com>
2023-09-12 09:50:47 +0300

    DB relations PTU16

--- PTU16_live/db/create_clients.sql
+++ PTU16_live/db/create_clients.sql
@@ -1,21 +1,42 @@
 -- SQLite
+CREATE TABLE address (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    street VARCHAR(100),
+    zip VARCHAR(10),
+    city VARCHAR(50),
+    country VARCHAR(50)    
+);
+
 CREATE TABLE client (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     first_name VARCHAR(50),
     last_name VARCHAR(50),
-    address VARCHAR(100),
     email VARCHAR(100),
-    phone VARCHAR(20)
+    phone VARCHAR(20),
+    address_id INTEGER UNIQUE NOT NULL REFERENCES address(id)
+);
+
+CREATE TABLE orders (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    date_time DATETIME,
+    quantity DECIMAL(10,2),
+    total DECIMAL(10,2),
+    client_id INTEGER NOT NULL REFERENCES client(id),
+    address_id INTEGER REFERENCES address(id)
 );
 
 -- ALTER TABLE client ADD phone VARCHAR(20);
 -- ALTER TABLE client DROP email;
 -- DROP TABLE client;
 
-INSERT INTO client (first_name, last_name, address, email, phone)
-    VALUES ("Kestutis", "Januskevicius", "Middle of Nowhere", "kestas@gka.lt", "+37069xxx411");
-INSERT INTO client (first_name, last_name, address, email, phone)
-    VALUES ("Piktas", "Trintukas", "Middle of Everywhere", "piktas@gka.lt", "nesakysiu");
+INSERT INTO address (street, zip, city, country) VALUES 
+    ("Middle of Everywhere", "92307", "Klaipėda", "Lithuania"),
+    ("Middle of Nowhere", "82820", "Maukkula", "Finland");
+
+INSERT INTO client (first_name, last_name, address_id, email, phone)
+    VALUES ("Kestutis", "Januskevicius", 2, "kestas@gka.lt", "+37069xxx411");
+INSERT INTO client (first_name, last_name, address_id, email, phone)
+    VALUES ("Piktas", "Trintukas", 1, "piktas@gka.lt", "nesakysiu");
 
 SELECT * FROM client;
 
---
+++ PTU16_live/db/knygos.sql
@@ -0,0 +1,29 @@
+CREATE TABLE knygos (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  pavadinimas VARCHAR(255) NOT NULL
+);
+
+-- Sukuriame "autoriai" lentelę
+CREATE TABLE autoriai (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  vardas VARCHAR(255) NOT NULL,
+  pavarde VARCHAR(255) NOT NULL
+);
+
+-- Sukuriame "knygos_autoriai" lentelę
+CREATE TABLE knygos_autoriai (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  knyga_id INTEGER REFERENCES knygos(id),
+  autorius_id INTEGER REFERENCES autoriai(id)
+);
+
+
+INSERT INTO knygos (pavadinimas) VALUES
+    ("Monk who sold his Ferrarri"), ("Who will cry when we die"),
+    ("Sprint: How to solve big problems and test new ideas in just five days");
+
+INSERT INTO autoriai (vardas, pavarde) VALUES
+    ("Robin", "Sharma"), ("Jake", "Knapp"), ("John", "Zeratsky"), ("Braden", "Kowitz");
+
+INSERT INTO knygos_autoriai (knyga_id, autorius_id) VALUES
+    (1, 1), (2, 1), (3, 3), (3, 4), (3, 5);
