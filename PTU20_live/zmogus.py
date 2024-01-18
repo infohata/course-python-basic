612236fcc368d5f46e3dc8fe489c4cdcae57689b
Kestutis Januskevicius <infohata@gmail.com>
2024-01-18 15:00:09 +0200

    PTU20 custom logger

--- PTU20_live/zmogus.py
+++ PTU20_live/zmogus.py
@@ -2,18 +2,27 @@ import pickle
 import os
 import logging
 
-logging.basicConfig(
-    filename='zmones.log', 
-    level=logging.DEBUG,
-    encoding='utf-8',
-    format='%(asctime)s / %(name)s / %(levelname)s / %(message)s'
-)
+def create_logger(file=__name__+".log", name=__name__, file_level=logging.DEBUG, console_level=logging.INFO):
+    logger = logging.getLogger(name)
+    logger.setLevel(file_level)
+    formatter = logging.Formatter('%(asctime)s|%(name)s-%(levelname)s|%(message)s')
+    file_handler = logging.FileHandler(file)
+    file_handler.setLevel(file_level)
+    file_handler.setFormatter(formatter)
+    console_handler = logging.StreamHandler()
+    console_handler.setLevel(console_level)
+    console_handler.setFormatter(formatter)
+    logger.addHandler(file_handler)
+    logger.addHandler(console_handler)
+    return logger
+
+logger = create_logger()
 
 class Zmogus:
     def __init__(self, vardas, amzius):
         self.vardas = vardas
         self.amzius = amzius
-        logging.debug(f"sukurtas Zmogus objektas: vardas: {self.vardas}, amzius: {self.amzius}")
+        logger.debug(f"sukurtas Zmogus objektas: vardas: {self.vardas}, amzius: {self.amzius}")
 
     def __str__(self):
         return f"{self.vardas}, {self.amzius} metų"
@@ -25,7 +34,7 @@ class AgeError(ValueError):
 
 def input_age(prompt="age: "):
     amzius = int(input(prompt))
-    logging.info(f"ivestas amzius: {amzius}")
+    logger.info(f"ivestas amzius: {amzius}")
     if amzius > 100:
         raise AgeError('zmones tiek negyvena')
     if amzius < 0:
@@ -37,7 +46,7 @@ if __name__ == "__main__":
         vardas = input("vardas: ")
         amzius = input_age("metai: ")
     except ValueError as error: ## arba Exception, arba AgeError
-        print(f'Klaida {error.__class__.__name__}: {error}')
+        logger.error(f'Klaida {error.__class__.__name__}: {error}')
         exit()
 
     zmogus = Zmogus(vardas, amzius)
